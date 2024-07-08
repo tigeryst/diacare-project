2a5af3a180e21026e0e760dfdbc83150cbb21d24
Tiger Yotsawat <tigeryotsawat@gmail.com>
2024-07-08 14:54:23 +0000

    Simplify setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,29 +2,6 @@
 
 echo "Starting setup script..."
 
-# Install Homebrew
-echo "Installing Homebrew..."
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Homebrew installation completed."
-
-# Add Homebrew to PATH
-echo "Adding Homebrew to PATH..."
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "Homebrew added to PATH."
-
-# Install pyenv using Homebrew
-echo "Installing pyenv with Homebrew..."
-brew install pyenv
-echo "pyenv installation completed."
-
-# Initialize pyenv
-echo "Initializing pyenv..."
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-echo "pyenv initialization completed."
-
 # Install a specific Python version using pyenv
 echo "Installing Python 3.10.0 with pyenv..."
 pyenv install -s 3.10.0
